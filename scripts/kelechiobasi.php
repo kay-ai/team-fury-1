c3f88b136de204158cd24c67fd76196031da9f11
kay-ai <favourobasi6@gmail.com>
2020-06-02 23:30:49 +0100

    Update kelechiobasi.php
    I

--- scripts/kelechiobasi.php
+++ scripts/kelechiobasi.php
@@ -3,5 +3,5 @@
     $id = "HNG-04349";
     $language = "PHP(Laravel)";
 
-    echo 'Hello World this is' . ' ' . '<b>' . $fullname . '</b>' . ' ' . 'with HNGi7 ID' . ' ' . '<b>' . $id . '</b>' . ' ' . 'using' . ' ' . '<b>' .$language . '<b>' . 'for stage 2 task';
+    echo 'Hello World this is' . ' ' . '<b>' . $fullname . '</b>' . ' ' . 'with HNGi7 ID' . ' ' . '<b>' . $id . '</b>' . ' ' . 'using' . ' ' . '<b>' .$language . '</b>' . 'for stage 2 task';
 ?>
