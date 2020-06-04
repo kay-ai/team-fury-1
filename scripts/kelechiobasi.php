90554ed181bec582df3324962a1c15bed8912a0c
kay-ai <favourobasi6@gmail.com>
2020-06-04 15:46:40 +0100

    Add to Scripts folder

--- scripts/kelechiobasi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $fullname = "Favour Kelechi Obasi";
-    $id = "HNG-04349";
-    $language = "Laravel";
-
-    echo 'Hello World this is' . ' ' . '<b>' . $fullname . '</b>' . ' ' . 'with HNGi7 ID' . ' ' . '<b>' . $id . '</b>' . ' ' . 'using' . ' ' . '<b>' .$language . '</b>' . 'for stage 2 task';
-?>
